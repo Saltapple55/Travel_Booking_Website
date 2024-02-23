1e14eefdcab0387586e9270dbf44577fe69e785e
biradon <truongchauminhjps15@gmail.com>
2024-02-23 01:43:38 -0500

    Update Search Dashboard

--- TravelGroupAssignment1/TravelGroupAssignment1/obj/Debug/net8.0/TravelGroupAssignment1.AssemblyInfo.cs
+++ TravelGroupAssignment1/TravelGroupAssignment1/obj/Debug/net8.0/TravelGroupAssignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e649c88248d9b20fe3fbce09f7f226f41136b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
