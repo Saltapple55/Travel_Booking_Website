591671f550ab1cdb3b7dbb105611c4120847722e
biradon <truongchauminhjps15@gmail.com>
2024-02-24 15:47:29 -0500

    Commit before take coffee

--- TravelGroupAssignment1/TravelGroupAssignment1/obj/Debug/net8.0/TravelGroupAssignment1.AssemblyInfo.cs
+++ TravelGroupAssignment1/TravelGroupAssignment1/obj/Debug/net8.0/TravelGroupAssignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+501b40a7edb732fad09b06d78ae03685ae31316d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8048fe94a10e57c221df1ca3a0a9ec04f09562bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
