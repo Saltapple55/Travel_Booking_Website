501b40a7edb732fad09b06d78ae03685ae31316d
biradon <truongchauminhjps15@gmail.com>
2024-02-24 13:42:39 -0500

    Update UI Car Index and Car Details

--- TravelGroupAssignment1/TravelGroupAssignment1/obj/Debug/net8.0/TravelGroupAssignment1.AssemblyInfo.cs
+++ TravelGroupAssignment1/TravelGroupAssignment1/obj/Debug/net8.0/TravelGroupAssignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7850c0ea5012c80304d14e2f8a8b8a05f5881a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5478fb8750bcbe36bcd2a08dc26a16987a21e4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
