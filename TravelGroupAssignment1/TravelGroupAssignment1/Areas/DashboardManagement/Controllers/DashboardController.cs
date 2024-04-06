accf6189aa8f671d80be9f5f23fb9a4d68f84303
biradon <truongchauminhjps15@gmail.com>
2024-04-06 12:48:25 -0400

    Create ViewModel for DashboardIndex Create DashboardIndex View Create Index method in DashboardController

--- TravelGroupAssignment1/TravelGroupAssignment1/Areas/DashboardManagement/Controllers/DashboardController.cs
+++ TravelGroupAssignment1/TravelGroupAssignment1/Areas/DashboardManagement/Controllers/DashboardController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using TravelGroupAssignment1.Areas.DashboardManagement.Models.ViewModel;
 using TravelGroupAssignment1.Data;
 using TravelGroupAssignment1.Models;
 
@@ -19,15 +20,32 @@ namespace TravelGroupAssignment1.Areas.DashboardManagement.Controllers
         {
             _context = context;
         }
-        // GET: HotelController
-        [HttpGet("HotelIndex")]
+
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index()
+		{
+			var model = new DashboardViewModel();
+
+			// Assuming you have DbContext set up
+			model.HotelCount = await _context.Hotels.CountAsync();
+			model.RoomCount = await _context.Rooms.CountAsync();
+			model.FlightCount = await _context.Flights.CountAsync();
+			model.CarRentalCompanyCount = await _context.CarRentalCompanies.CountAsync();
+			model.CarCount = await _context.Cars.CountAsync();
+            // Will comment out after have the user table
+			// model.UserCount = await _context.Users.CountAsync();
+			return View(model);
+		}
+
+		// ========================== HOTEL ============================
+		// GET: HotelController
+		[HttpGet("HotelIndex")]
         public async Task<IActionResult> HotelIndex()
         {
             var hotels = await _context.Hotels.ToListAsync();
             return View(hotels);
         }
 
-        // ========================== HOTEL ============================
         // GET: HotelController/Details/5
         [HttpGet("HotelDetails/{id:int}")]
         public async Task<IActionResult> HotelDetails(int id)
