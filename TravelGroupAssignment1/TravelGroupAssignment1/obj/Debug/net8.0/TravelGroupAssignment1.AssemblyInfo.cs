3bfb946b16cda58dd44e6dd13b41dbe0d4a27e6c
biradon <truongchauminhjps15@gmail.com>
2024-04-14 02:16:45 -0400

    Commit after pull, finish the authorization

--- TravelGroupAssignment1/TravelGroupAssignment1/obj/Debug/net8.0/TravelGroupAssignment1.AssemblyInfo.cs
+++ TravelGroupAssignment1/TravelGroupAssignment1/obj/Debug/net8.0/TravelGroupAssignment1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ac9ff863fe7ef710c7535a07935fb36bc183fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
