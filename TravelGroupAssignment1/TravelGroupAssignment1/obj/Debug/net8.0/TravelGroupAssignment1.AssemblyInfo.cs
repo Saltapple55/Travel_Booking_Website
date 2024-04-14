a096fe1473bdf83a11b1b2c76f0c4a49d239e2d0
biradon <truongchauminhjps15@gmail.com>
2024-04-14 01:16:52 -0400

    Authorization for Room

--- TravelGroupAssignment1/TravelGroupAssignment1/obj/Debug/net8.0/TravelGroupAssignment1.AssemblyInfo.cs
+++ TravelGroupAssignment1/TravelGroupAssignment1/obj/Debug/net8.0/TravelGroupAssignment1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc7bce067a729725eb3bf79ee8dfbd0bf5d64aa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcde0b5b07505a5da7fa3c0d724a7cc63ddd888b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelGroupAssignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
