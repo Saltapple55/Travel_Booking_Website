e61c8ae1ba39d0237e1440af68ba0dbb5d90f469
biradon <truongchauminhjps15@gmail.com>
2024-04-14 21:39:17 -0400

    Update DbContext

--- TravelGroupAssignment1/TravelGroupAssignment1/Data/ApplicationDbContext.cs
+++ TravelGroupAssignment1/TravelGroupAssignment1/Data/ApplicationDbContext.cs
@@ -1,11 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore;
+using TravelGroupAssignment1.Areas.CarManagement.Models;
+using TravelGroupAssignment1.Areas.DashboardManagement.Models.ViewModels;
+using TravelGroupAssignment1.Areas.HotelManagement.Models;
+using TravelGroupAssignment1.Areas.RoomManagement.Models;
+=========
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using TravelGroupAssignment1.Areas.CarManagement.Models;
-using TravelGroupAssignment1.Areas.CustomerManagement.Models;
-using TravelGroupAssignment1.Areas.FlightManagement.Models;
-using TravelGroupAssignment1.Areas.HotelManagement.Models;
-using TravelGroupAssignment1.Areas.RoomManagement.Models;
+>>>>>>>>> Temporary merge branch 2
 using TravelGroupAssignment1.Models;
 namespace TravelGroupAssignment1.Data
 {
