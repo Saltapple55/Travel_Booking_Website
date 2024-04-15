7c411466d46915ee5eef3391777d1a3b34aa58c7
biradon <truongchauminhjps15@gmail.com>
2024-04-15 19:13:55 -0400

    First Deploymen - success

--- TravelGroupAssignment1/TravelGroupAssignment1/Migrations/20240415212145_mssql_migration_620.Designer.cs
+++ TravelGroupAssignment1/TravelGroupAssignment1/Migrations/20240415212145_mssql_migration_620.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TravelGroupAssignment1.Data;
 
@@ -11,9 +12,11 @@ using TravelGroupAssignment1.Data;
 namespace TravelGroupAssignment1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240415212145_mssql_migration_620")]
+    partial class mssql_migration_620
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
